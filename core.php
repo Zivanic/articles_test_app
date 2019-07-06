f9455391f6bab89ffea79fcb7e19adf9eae149e6
Zivanic <zivanic23@gmail.com>
2019-07-06 16:58:59 +0200

    Finished navigation

---
+++ core.php
@@ -0,0 +1,11 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+ 
+// start php session
+session_start();
+
+// home page url
+$home_url="http://localhost/articles_test_app/";
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
+// core configuration
+include_once "core.php";
+
+// set page title
+$page_title = "Create article";
+
+// include page header HTML
+include_once 'navigation.php';
+
 echo "Articles test app";
